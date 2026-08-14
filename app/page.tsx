90371ec59fe033cf90bf8f1c5413efb5a2d160a8
Alan Geirnaert <geirnaert.alan@icloud.com>
2026-08-14 23:11:02 +0200

    fix: improved design a bit

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default function App() {
     <div>
       <h1 className="text-center text-2xl py-4">Entropy Chess</h1>
 
-      <div className="grid grid-cols-3 gap-2 h-full px-8">
-        <div className="col-span-2 flex items-center justify-center">
+      <div className="grid grid-cols-2 gap-2 h-full px-8">
+        <div className="col-span-1 flex items-center justify-center">
           <BoardComponent turn={turn} setTurn={setTurn} lostPieces={lostPieces} setLostPieces={setLostPieces}></BoardComponent>
         </div>
 
--- components/LostPieces.tsx
+++ components/LostPieces.tsx
@@ -11,17 +11,19 @@ export default function LostPiecesComponent({ lostPieces }: { lostPieces: Piece[
   }, [lostPieces]);
 
   return (
-    <div className="flex flex-col justify-between border w-full h-full">
-      <div>
-        {whiteLostPieces.map((piece: Piece, index: number) => (
-          <Image key={index} src={piece.imageUrl} alt={piece.name + piece.side} width={30} height={30}></Image>
-        ))}
-      </div>
+    <div className="flex flex-col justify-between border w-full h-full p-2 bg-foreground">
+      <div className="border w-full h-full rounded-xl bg-background">
+        <div>
+          {whiteLostPieces.map((piece: Piece, index: number) => (
+            <Image key={index} src={piece.imageUrl} alt={piece.name + piece.side} width={30} height={30}></Image>
+          ))}
+        </div>
 
-      <div>
-        {blackLostPieces.map((piece: Piece, index: number) => (
-          <Image key={index} src={piece.imageUrl} alt={piece.name + piece.side} width={30} height={30}></Image>
-        ))}
+        <div>
+          {blackLostPieces.map((piece: Piece, index: number) => (
+            <Image key={index} src={piece.imageUrl} alt={piece.name + piece.side} width={30} height={30}></Image>
+          ))}
+        </div>
       </div>
     </div>
   )
